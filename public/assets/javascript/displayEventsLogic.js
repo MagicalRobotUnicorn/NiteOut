6a8d4b5394245fde18d648f18444758b51884228
John Weber <johnhenryweber@gmail.com>
2019-09-10 06:48:52 -0700

    Events Display Logic Finished - Beta

--- public/assets/javascript/displayEventsLogic.js
+++ public/assets/javascript/displayEventsLogic.js
@@ -4,9 +4,41 @@ function displayEvents() {
   if (storedEvents != null) {
     $('#eventsDisplay').html('');
 
-    for (var i = 0; i < storedEvents.length; i++){
-      
+    for (var i = 0; i < storedEvents.length; i++) {
+      var $newRow = $('<div class="row">');
+      var concertId = storedEvents[i].concertId;
+      var restaurantId = storedEvent[i].restaurantId;
+      var eventDetails;
+      var restaurantDetails;
+
+      var queryURL = './api/eventdetails?idNumber=' + concertId;
+      $.ajax({
+        url: queryURL,
+        method: 'GET'
+      }).then(function (response) {
+        eventDetails = response;
+
+        var secondQuery = './api/restaurantdetails?idNumber=' + restaurantId;
+        $.ajax({
+          url: secondQuery,
+          method: 'GET'
+        }).then(function (response) {
+          restaurantDetails = response;
+          
+          var concertDisplay = eventDetails.displayName;
+          var restaurantDisplay = restaurantDetails.name;
+
+          var $concertP = $('<p>');
+          var $restaurantP = $('<p>');
+          $concertP.append(concertDisplay);
+          $restaurantP.append(restaurantDisplay);
+          $newRow.append($concertP);
+          $newRow.append($restaurantP);
+
+          $('#eventsDisplay').append($newRow);
+        });
+
+      })
     }
   }
-
 }
